6b156d7bd3a5ac1812304e97cef871f69098d7a6
Manish Sharma <Manish.Sharma@bafflesol.com>
2020-08-18 12:57:29 +0530

    global methods part 2

--- global.js
+++ global.js
@@ -1,10 +1,13 @@
 //console.log(global);
 
-setTimeout(() => {
-    console.log('in the timeout');
-    clearInterval(interval);
-}, 3000);
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(interval);
+// }, 3000);
 
-const interval = setInterval(() => {
-    console.log("every second");
-}, 1000);
+// const interval = setInterval(() => {
+//     console.log("every second");
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
