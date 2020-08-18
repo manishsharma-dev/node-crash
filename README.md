ecd94672b649a19d57b221c9ea14af0dd02a2e6b
Manish Sharma <Manish.Sharma@bafflesol.com>
2020-08-18 16:04:14 +0530

    create server

--- README.md
+++
@@ -1,3 +0,0 @@
-# node-crash
-
-node crash course
--- people.js
+++
@@ -1,8 +0,0 @@
-const people = ['Rohit','Dhawan','Kohli','Manish'];
-const ages = [32,33,31,29];
-
-console.log(people);
-
-module.exports = {
-    people , ages
-};
