bd7adb77e8ccb243b63b1b2b4fe38a7284dfe2ee
Manish Sharma <Manish.Sharma@bafflesol.com>
2020-08-18 14:00:20 +0530

    create & remove directories

--- files.js
+++ files.js
@@ -11,16 +11,27 @@ const fs = require('fs');
 
 //write files
 
-fs.writeFile('./docs/blog.txt','hello, world.', () => {
- console.log('file written');
-})
-fs.writeFile('./docs/blog1.txt','hello, world.', () => {
- console.log('file written');
-})
+// fs.writeFile('./docs/blog.txt','hello, world.', () => {
+//  console.log('file written');
+// })
+// fs.writeFile('./docs/blog1.txt','hello, world.', () => {
+//  console.log('file written');
+// })
 
 
 //directories
-
+if(!fs.existsSync('./assets')){
+fs.mkdir('./assets', (err) => {
+    if(err) console.log(err);
+    console.log('folder created');
+});
+}
+else{
+    fs.rmdir('./assets', (err) => {
+        if(err) console.log(err);
+        console.log('folder deleted');
+    })
+}
 
 
 //delete files
