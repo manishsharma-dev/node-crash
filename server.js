e71eb336a2d411a45f0430687c34d5b6df498d8d
Manish Sharma <Manish.Sharma@bafflesol.com>
2020-08-18 16:20:38 +0530

    send responses

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<head><link rel = "stylesheet" href ="#"></head>');
+    res.write('<p>Hello</p>');
+    res.write('<h1>All</h1>');
+    res.end();
 });
 
 server.listen(3000,'localhost', ()=>{
