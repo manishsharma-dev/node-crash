120e1b93a1f7cdcc15602922f28005a2a64716f2
Manish Sharma <Manish.Sharma@bafflesol.com>
2020-08-18 13:53:50 +0530

    write files

--- files.js
+++ files.js
@@ -2,15 +2,21 @@ const fs = require('fs');
 
 //read files
 
-fs.readFile('./docs/blog.txt', (err, data) => {
-    if(err) console.log(err);
-    console.log(data.toString());
-});
+// fs.readFile('./docs/blog.txt', (err, data) => {
+//     if(err) console.log(err);
+//     console.log(data.toString());
+// });
 
-console.log('last line');
+// console.log('last line');
 
 //write files
 
+fs.writeFile('./docs/blog.txt','hello, world.', () => {
+ console.log('file written');
+})
+fs.writeFile('./docs/blog1.txt','hello, world.', () => {
+ console.log('file written');
+})
 
 
 //directories
