8a4eaa5dc695ee6a19dbde3c751388c6caca99e1
Manish Sharma <Manish.Sharma@bafflesol.com>
2020-08-19 17:28:56 +0530

    ejs basics

--- app.js
+++ app.js
@@ -3,27 +3,38 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engines
+
+app.set('view engine', 'ejs');
+
 //listen for request
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<p>Home Page</p>');
-    res.sendFile('./views/index.html' , { root: __dirname });
+    //res.sendFile('./views/index.html' , { root: __dirname });
+    const blogs = [
+        {title:'manish finds eggs' , snippet:'Lorem ipsum dolor sit amet'},
+        {title:'light finds stars' , snippet:'Ut enim ad minim veniam'},
+        {title:'ryuk defeats all' , snippet:'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'}
+    ]
+    res.render('index' ,{title : 'Home',blogs});
 })
 
 app.get('/about', (req, res) => {
   //res.send('<p>About Page</p>');
-   res.sendFile('./views/about.html' , { root: __dirname });
+   //res.sendFile('./views/about.html' , { root: __dirname });
+   res.render('about',{title : 'About'} );
 })
 
-// redirects
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about');
+app.get('/blogs/create',(req,res) =>{
+    res.render('create',{title : 'Create new blog'});
 })
 
 //404
 
 app.use( (req,res)=>{
-    res.status(404).sendFile('./views/404.html' , { root: __dirname });
+    // res.status(404).sendFile('./views/404.html' , { root: __dirname });
+    res.status(404).render('404',{title : '404'});
 })
