6670a5e64779b2ba495011e6809a2509ef5d221f
Manish Sharma <Manish.Sharma@bafflesol.com>
2020-09-04 12:19:40 +0530

    crud operations

--- app.js
+++ app.js
@@ -22,57 +22,55 @@ app.set('view engine', 'ejs');
 //middleware and static files
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 
-//mongoose and mongo sandbox routes
-
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new Blog 2',
-        snippet: 'about new blog 2',
-        body: 'more about my new blog 2'
-    });
-    blog.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((results) => {
-            res.send(results);
-        })
-        .catch((err) => console.log(err));
-})
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('5f4decde2624be2e3c6e8744')
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => console.log(err));
-})
-
 app.get('/', (req, res) => {
-    //res.send('<p>Home Page</p>');
-    //res.sendFile('./views/index.html' , { root: __dirname });
-    const blogs = [
-        { title: 'ryujaki finds Kira', snippet: 'Lorem ipsum dolor sit amet' },
-        { title: 'light finds rem', snippet: 'Ut enim ad minim veniam' },
-        { title: 'ryuk defeats all', snippet: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' }
-    ]
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 })
 
 app.get('/about', (req, res) => {
     //res.send('<p>About Page</p>');
     //res.sendFile('./views/about.html' , { root: __dirname });
     res.render('about', { title: 'About' });
+})
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result })
+        })
+        .catch((err) => console.log(err));
+});
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => console.log(err));
+});
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', { blog: result, title: 'Blog Details' })
+        })
+        .catch((err) => console.log(err));
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({
+                redirect : '/blogs'
+            })
+        })
+        .catch((err) => console.log(err));
 })
 
 app.get('/blogs/create', (req, res) => {
